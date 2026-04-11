6953c531e380ed9387953c49caee49d3ed74394a
IslesPJ <isles.pj@gmail.com>
2026-04-11 21:59:15 +0900

    All features completed.

--- script.js
+++ script.js
@@ -14,100 +14,127 @@ let hasDecimal = false,
     num1 = "",
     num2 = "",
     symbol = "",
-    ans = "";
-
-function sum(a,b) {return a + b};
-function difference(a,b) {return a - b};
-function product(a,b) {return a * b};
-function quotient(a,b) {return a / b};
+    resetTrigger = false;
+    
 
 function operate(a,b,operator) {
+    let ans = "";
     switch (operator) {
-        case " + ":
-            ans = sum(a,b);
+        case "+":
+            ans = a + b;
             break;
-        case " - ":
-            ans = difference(a,b);
+        case "-":
+            ans = a - b;
             break;
-        case " × ":
-            ans = product(a,b);
+        case "×","*":
+            ans = a * b;
             break;
-        case " ÷ ":
-            b == 0? ans = "You cannot do that." : 
-            ans = quotient(a,b);
+        case "÷","/":
+            b == 0? ans = "oops!" : 
+            ans = a / b;
             break;
     }
-    input.value = ans;
-    num1 = ans;
-    num2 = num2;
-    if (num1 % 1 != 0) hasDecimal = true;
+    return ans;
+};
+
+decimal.addEventListener("click", decimalHandler);
+function decimalHandler() {
+    if (!hasDecimal) {
+            input.textContent += ".";
+            hasDecimal = true;
+    }
+};
+
+function shorten() {                                //unfinished
+    if (input.textContent.length == 14) {
+        input.textContent.slice(0,-1);
+    }
 };
 
 numbers.forEach((num) => {
-    num.addEventListener("click", (e) => { 
-        if (!ans == "") reset();
-        if (e.target.value == "." && hasDecimal) {
-            // Do nothing;
-        }
-        else if (e.target.value == "." && !hasDecimal) {
-            input.value += e.target.value;
-            hasDecimal = true;
-        }
-        else {
-            input.value += e.target.value;
-        }
-        if (symbol == "") {
-                num1 = input.value;
-            }
-        else {
-                num2 = input.value;
-            }
-    })
+    num.addEventListener("click", numberHandler);
 });
+function numberHandler(e) {
+    if (resetTrigger) {reset(); resetTrigger = false;}
+    let eventType;
+    typeof e === "string" ? eventType = e: eventType = e.target.value;
 
-operators.forEach(operator => {
-    operator.addEventListener("click", (e) => {
-        if (num1 == "") {
-            input.value = "";
-        }
-        else {
-            hasDecimal = false;
-            input.value = "";
-            symbol = e.target.value;
-            log.textContent = num1 + e.target.value;
-            if (!symbol == "" && !num2 == "") {equal()}
-        }
-        
-    })
-});
-const equal = function() {
-    hasDecimal = false;
-    log.textContent = num1 + symbol + num2;
-    num1 = Number(num1);
-    num2 = Number(num2);
-    operate(num1,num2,symbol);
-};
+    input.textContent += eventType;
 
-equals.addEventListener("click", () => {
-    if (num1 == "" || symbol == "" || num2 == "") {
-        input.value = "";
+    if (symbol == "") {
+            num1 = input.textContent;
     }
     else {
-        equal();
+            num2 = input.textContent;
     }
+};
+
+operators.forEach(operator => {
+    operator.addEventListener("click", operatorHandler )
 });
+function operatorHandler(e) {
+    let eventType;
+    typeof e === "string" ? eventType = e: eventType = e.target.value;
+
+    if (num1 == "") {
+        input.textContent = "";
+    }
+    else {
+        hasDecimal = false;
+        input.textContent = "";
+        log.textContent = num1 + eventType;
+        if (!symbol == "" && !num2 == "") {
+            equal();
+            operatorHandler(e);
+        }
+        symbol = eventType;
+    }
+    resetTrigger = false;
+}
+
+equals.addEventListener("click", equal);
+function equal() {
+    if (num1 == "" || symbol == "" || num2 == "") {
+        reset();
+    }
+    else {
+        hasDecimal = false;
+        log.textContent = num1 + symbol + num2;
+        num1 = Number(num1);
+        num2 = Number(num2);
+        input.textContent = operate(num1,num2,symbol);
+        num1 = input.textContent;
+        num2 = "";
+        symbol = "";
+        resetTrigger = true;
+        if (num1 % 1 != 0) hasDecimal = true;
+        if (num1 == "oops!") alert("Cannot divide by Zero!");
+    }
+};
+
 
 backspace.addEventListener("click", (e) => {
-    input.value = input.value.slice(0,-1);
+    input.textContent = input.textContent.slice(0,-1);
 });
 
 clear.addEventListener("click", reset);
-
 function reset() {
-    input.value = "";
+    input.textContent = "";
     log.textContent = "";
     num1 = "";
     num2 = "";
     symbol = "";
     ans = "";
-}
+    hasDecimal = false;
+};
+
+// keyboard support
+document.addEventListener("keydown", (e) => {
+    e.preventDefault();
+    let key = e.key;
+    if (key === "Backspace") {input.textContent = input.textContent.slice(0,-1);}
+    else if (key === "Enter") {equal()}
+    else if (key === ".") {decimalHandler()}
+    else if (["0","1","2","3","4","5","6","7","8","9",].includes(key)) {numberHandler(key)}
+    else if (["+","-","*","/"].includes(key)) {operatorHandler(key)}
+});
